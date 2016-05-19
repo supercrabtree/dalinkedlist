0d48527dfd7c332c455c3f3f2984b263a7819b12
George Crabtree <supercrabtree@googlemail.com>
2016-05-19 09:49:41 +0200

    Setup linked list shell

---
+++ index.js
@@ -0,0 +1,53 @@
+module.exports = function dalinkedlist() {
+  'use strict';
+
+
+  // the private internal array the linked list will proxy
+
+  var dataStore;
+
+  var length;
+  var head;
+
+
+  var linkedList = {
+
+    // properties
+
+    get size() {
+
+    },
+
+    // methods
+    append: function () {
+      
+    },
+
+    insert: function () {
+      
+    },
+
+    removeAt: function () {
+      
+    },
+
+    remove: function () {
+      
+    },
+
+    indexOf: function () {
+      
+    },
+
+    toString: function () {
+      
+    },
+
+    toArray: function () {
+      
+    }
+
+  };
+
+  return Object.freeze(linkedList);
+};
